0392bffa96f564e1075fa892eb69949c30f5355f
sifte <84506518+sifte@users.noreply.github.com>
2022-04-28 08:05:46 +0800

    Update constants.py

--- helpers/constants.py
+++ helpers/constants.py
@@ -7,7 +7,7 @@ class Config:
     MONGO="mongodb+srv://kayle:kaylebetter@cluster0.s0wqa.mongodb.net/discord?retryWrites=true&w=majority"
     KEY="c8b299ee91msha4c1921cebbedabp1f70a0jsnc4c1cc4b9c5c"
     HOST="mashape-community-urban-dictionary.p.rapidapi.com"
-    TOKEN="ODg4MzA5OTE1NjIwMzcyNDkx.YUQ1Ew.kRuP4WKEdgrNDJaiYS-SZLUAEmo"
+    TOKEN="ODg4MzA5OTE1NjIwMzcyNDkx.YUQ1Ew.W_7Ts_-tZTnQc4nMbzn8jHnht8Q"
 
 REPLIES = [ 
     "Noooooo!!",
